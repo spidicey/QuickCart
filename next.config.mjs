810c63fec9e568dcbd533a74d7c69548022248d9
spidicey <0949799341a@gmail.com>
2025-12-29 14:41:55 +0700

    turn off error

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "**",
-        },
-        {
-          protocol: "http",
-          hostname: "**",
-        },
-      ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
     unoptimized: true,
   },
   devIndicators: {
-    buildActivity: false,
     appIsrStatus: false,
   },
-  // Disable error overlay
   reactStrictMode: false,
 };
 
